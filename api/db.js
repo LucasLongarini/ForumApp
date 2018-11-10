1e753b1f88640b4d9ce7f4ed8616d51db854fc58
Lucas Longarini <lucaslongarini@Lucass-Mac-mini.local>
2018-11-10 15:35:47 -0700

    user clear db

--- api/db.js
+++ api/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "samson",
-    database: "mydb"
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b9722f7bd3eaa8",
+    password: "c883147f",
+    database: "heroku_e5f64c95cb1c6a1"
 })
 
 connection.connect((err)=>{
