509cab15910967a7580eb1930b35d959fac57288
Lucas Longarini <lucaslongarini@Lucass-Mac-mini.local>
2018-11-10 16:50:50 -0700

    fix

--- api/db.js
+++ api/db.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql')
 
 var connection = mysql.createConnection({
+    connectionLimit: 10,
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "b9722f7bd3eaa8",
     password: "c883147f",
