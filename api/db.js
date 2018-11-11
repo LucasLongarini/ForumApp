275acb60ecb53760cd9994d6d062320906609f7e
Lucas Longarini <lucaslongarini@Lucass-Mac-mini.local>
2018-11-10 17:05:48 -0700

    fix1

--- api/db.js
+++ api/db.js
@@ -1,16 +1,24 @@
 const mysql = require('mysql')
 
-var connection = mysql.createConnection({
-    connectionLimit: 10,
+// var connection = mysql.createConnection({
+//     connectionLimit: 10,
+//     host: "us-cdbr-iron-east-01.cleardb.net",
+//     user: "b9722f7bd3eaa8",
+//     password: "c883147f",
+//     database: "heroku_e5f64c95cb1c6a1"
+// })
+
+// // connection.connect((err)=>{
+// //     if (err) throw err
+// //     console.log('Connected to MySQL')
+// // })
+
+const connection = mysql.createPool({
+    connectionLimit:10,
     host: "us-cdbr-iron-east-01.cleardb.net",
     user: "b9722f7bd3eaa8",
     password: "c883147f",
     database: "heroku_e5f64c95cb1c6a1"
-})
-
-connection.connect((err)=>{
-    if (err) throw err
-    console.log('Connected to MySQL')
 })
 
 module.exports = connection;
